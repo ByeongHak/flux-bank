33cd55bdc3dcfc6ea3edb09c8bce5f0258547de1
Byeong-Hak, Lee <pro87kr@hanmail.net>
2018-01-29 17:42:13 +0900

    2단계, 플럭스 유틸 적용(Store)

--- app/BankBalanceStore.js
+++ app/BankBalanceStore.js
@@ -1,49 +1,42 @@
 /**
  * 스토어
+ * (1단계)
  * - 소유한 상태를 저장하며 자신을 디스패처에 등록.
  * - 액션이 발송될때마다 모든 스토어가 호출되며 해당 액션과 연관되는지 여부 결정.
  * - 연관되는 경우 스토어는 내부 상태를 변경하고 이벤트를 생성해 스토어가 변경된 것을
  *  뷰에 알림
  * - 이벤트를 내보내기 위해 이벤트 방출기(event emitter)패키지 : fbemitter 설치
+ * (2단계) 플럭스 유틸 스토어를 활용
+ *  - 이벤트 방출기의 인스턴스를 직접 만들 필요가 없다.
+ *  - 수동으로 스토어를 디스패치에 등록하지 않는다. 대신 스토어의 인스턴스를 생성하고 이를 디스패처에 인수로 전달한다.
  */
-import {EventEmitter} from 'fbemitter';
+import {Store} from 'flux/utils';
 import AppDispatcher from './AppDispatcher';
 import bankConstants from './constants';
 
-const CHANGE_EVENT = 'change';
-let __emitter = new EventEmitter();
 let balance = 0;
 
-/**
- * BankBalanceStore의 기본구조
- * 이벤트 방출기 인스턴스를 생성하고 스토어 변경 이벤트를 구독하기 위한
- * addListener 메서드를 제공, 애플리케이션 디스패처를 임포트하며 발송된
- * 모든 액션에 대해 호출되는 콜백을 제공하고 스토어에 등록.
- */
-let BankBalanceStore = {
-  getState() {
+class BankBalanceStore extends Store {
+  getState(){
     return balance;
-  },
-  addListener: (callback) => {
-    return __emitter.addListener(CHANGE_EVENT, callback)
-  },
-};
-
-BankBalanceStore.dispatchToken = AppDispatcher.register((action) => {
-  switch(action.type){
-    case bankConstants.CREATED_ACCOUNT:
-      balance = 0;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.DEPOSITED_INTO_ACCOUNT:
-      balance =   balance + action.ammount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.WITHDREW_FROM_ACCOUNT:
-      balance = balance - action.ammount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
   }
-});
 
-export default BankBalanceStore;
+  __onDispatch(action){
+    switch(action.type){
+      case bankConstants.CREATED_ACCOUNT:
+        balance = 0;
+        this.__emitChange();
+        break;
+      case bankConstants.DEPOSITED_INTO_ACCOUNT:
+        balance =   balance + action.ammount;
+        this.__emitChange();
+        break;
+      case bankConstants.WITHDREW_FROM_ACCOUNT:
+        balance = balance - action.ammount;
+        this.__emitChange();
+        break;
+    }
+  }
+}
+
+export default new BankBalanceStore(AppDispatcher);
